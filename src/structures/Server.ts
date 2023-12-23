6a8392d92bdfd1f7b3850d6e12c232069c6715a9
polygon6233 <88374836+polygon6233@users.noreply.github.com>
2023-12-23 18:40:56 +0000

    Fixed express server

--- src/structures/Server.ts
+++ src/structures/Server.ts
@@ -1,12 +1,23 @@
-import * as express from "express";
+import express from 'express';
+import "colorts/lib/string.js"
 
-const server = express();
-server.all("/", (res) => {
-    res.send("Server online!");
+const app: express.Application = express();
+const port: number = 6233;
+let fullURL: string;
+
+// Handling '/' Request
+app.get('/', (_req, _res) => {
+    fullURL = _req.protocol + '://' + _req.get('host') + _req.originalUrl;
+    _res.send("Server online!");
+});
+
+// Server setup
+app.listen(port, () => {
+    console.log(`App listening on ${fullURL}:${port}`);
 });
 
 export function keepAlive() {
-    server.listen(3000, () => {
+    app.listen(3000, () => {
         console.log("Server is online!".green.bold)
     })
 }
