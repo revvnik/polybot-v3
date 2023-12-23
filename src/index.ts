0471877c1717ee02f0b7e6c0c464c91ccceb9f3f
polygon6233 <nikitasitter@gmail.com>
2023-12-23 19:15:17 +0100

    Added keepAlive function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { ExtendedClient } from './structures/PolyBot.js';
+import { keepAlive } from './structures/Server.js';
 
 const client = new ExtendedClient();
 client.start();
+keepAlive();
---
+++ src/structures/Server.ts
@@ -0,0 +1,12 @@
+import { express } from "express";
+
+const server = express();
+server.all("/", (req, res) => {
+    res.send("Server online!");
+});
+
+export function keepAlive() {
+    server.listen(3000, () => {
+        console.log("Server is online!".green.bold)
+    })
+}
