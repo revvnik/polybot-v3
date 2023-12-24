c06a965270b1a2e3ff989de855667c2ab7e99f33
polygon6233 <nikitasitter@gmail.com>
2023-12-24 11:10:17 +0100

    Deleted firebase functions

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,3 @@
 import { ExtendedClient } from './structures/PolyBot.js';
-import { keepAlive } from './structures/Server.js';
 const client = new ExtendedClient();
 client.start();
-keepAlive();
--- src/functions/index.js
+++
@@ -1,4 +0,0 @@
-import { ExtendedClient } from '..//structures/PolyBot.js';
-
-const client = new ExtendedClient();
-client.start();
