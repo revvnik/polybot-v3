e5b72b2eb5e3e7f8c0f63637cc3736fb67833195
polygon6233 <nikitasitter@gmail.com>
2024-03-25 14:32:31 +0100

    Modified command deployment to match host startup command

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
 import { ActivityType } from 'discord.js';
 import { ExtendedClient } from './structures/Revver.js';
 import { Server } from "./structures/Server.js";
+import { Deployer } from './structures/Deployer.js';
 // import { uploadDistToRemote } from './miscellaneous/util.js';
-
 /*
 if(process.cwd() == "G:\\Revver") {
     await uploadDistToRemote();
 }
 */
 
+
 export const client = new ExtendedClient();
 await client.start();
 
 export const server = new Server();
 server.startServer();
+
+export const deployer = new Deployer();
+deployer.deployCommands();
 
 client.on("ready", () => {
     console.log(`${client.user.username} is online!`.green.bold);
