13f98040182f6e4ba850f031b875182ab3f2e41c
polygon6233 <nikitasitter@gmail.com>
2024-04-01 12:29:42 +0200

    Added userinfo context command

---
+++ src/commands/context/userinfo.ts
@@ -0,0 +1,46 @@
+import { ApplicationCommandType, ContextMenuCommandBuilder, EmbedBuilder, type MessageContextMenuCommandInteraction } from 'discord.js';
+
+
+
+import type { Command } from '../../structures/types/Command.js';
+import moment from 'moment';
+
+const userinfoCommand: Command = {
+    build() {
+        return new ContextMenuCommandBuilder()
+            .setName("userinfo")
+            .setType(ApplicationCommandType.User)
+    },
+    opt: {
+        userPermissions: ['SendMessages'],
+        botPermissions: ['SendMessages'],
+        category: 'Context',
+        cooldown: 5
+    },
+    async execute(interaction: MessageContextMenuCommandInteraction<'cached'>) {
+        const selectedUser = await interaction.user.fetch();
+
+        const userInfoEmbed = new EmbedBuilder()
+            .setAuthor({ name: `Userinfo of ${selectedUser.username}`, iconURL: selectedUser.displayAvatarURL()})
+            .setTitle("Avatar URL")
+            .setColor("Random")
+            .setURL(selectedUser.displayAvatarURL())
+            .addFields(
+                {
+                    name: "ID",
+                    value: `${selectedUser.id}`,
+                    inline: true
+                },
+                {
+                    name: "Joined Discord at:",
+                    value: `${moment(selectedUser.createdAt).format("DD-MM-YYYY HH:mm:ss")}`,
+                    inline: true
+                }
+            );
+
+        await interaction.reply({
+            embeds: [userInfoEmbed]
+        });
+    }
+}
+export default userinfoCommand;
